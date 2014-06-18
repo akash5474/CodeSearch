e4e3d6b8f96040bd6452e6c02e2e44bd7d2f7720
buchanaf <buchanaf@gmail.com>
2014-06-18 18:18:27 -0400

    HTTP posting for URL

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,12 @@
 
 angular.module('codeSearchApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    $scope.fileUrl = {url: ''};
+
+    $scope.addFile = function () {
+      $http.post('/api/addFile', $scope.fileUrl)
+        .success(function(data){
+          console.log(data)
+        })
+    };
   });
