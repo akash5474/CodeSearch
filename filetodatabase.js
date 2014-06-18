323e2afde2c47f19df13b2f1b87381e9a77f9a66
Jessica <jmw511@nyu.edu>
2014-06-18 15:52:44 -0400

    file to database script

---
+++ filetodatabase.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+var extractDependencies = function(content) {
+ return content.match(/require\(\s*[\'\"][a-z]*[\'\"]\s*\)/g)
+               .map(function(el) {
+                 var finStr = el.substring(8, el.length - 1).trim();
+                 return finStr.substring(1, finStr.length - 1);
+               });
+};
+
+fs.readFile('./server.js', function(err, data) {
+ if ( err ) throw err;
+
+ var content = data.toString();
+
+ var depsMapped = extractDependencies(content);
+
+ console.log(depsMapped);
+});
