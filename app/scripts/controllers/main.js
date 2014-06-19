55f36549ee76319c498b1f048c47842e17c7c8ef
buchanaf <buchanaf@gmail.com>
2014-06-19 11:07:33 -0400

    Added search route and forms

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,18 @@
 angular.module('codeSearchApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.fileUrl = {url: ''};
+    $scope.findLibrary = {library:''}
+    $scope.findFunction = {function:''}
 
     $scope.addFile = function () {
       $http.post('/api/addFile', $scope.fileUrl)
+        .success(function(data){
+          console.log(data)
+        })
+    };
+
+    $scope.findCode = function () {
+      $http.get('/api/findFile', $scope.findLibrary)
         .success(function(data){
           console.log(data)
         })
