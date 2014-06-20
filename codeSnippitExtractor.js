79468fcfeb939a8a2b2099c9f4afc98f8367dd6c
Akash Pannu <akash5474@gmail.com>
2014-06-20 13:15:02 -0400

    Modified snippit extractor to account for differences in variable names.

--- codeSnippitExtractor.js
+++ codeSnippitExtractor.js
@@ -1,8 +1,30 @@
 var fs = require('fs');
 
+module.exports = function(content, libQuery, fnQuery) {
 
-module.exports = function(content, query) {
-  var startIndex = content.indexOf(query);
+  var regex = new RegExp("require\\(\\s*[\\'\\\"]"+ libQuery +"[\\'\\\"]\\s*\\)");
+
+  var libIndex = content.match(regex);
+
+  var libVar = [];
+  var foundLib = false;
+
+  for ( var i = libIndex.index - 1; i >= 0; i-- ) {
+    if ( content[i] !== '=' && content[i] !== ' ' ) {
+      foundLib = true;
+      libVar.unshift( content[i] );
+    }
+
+    if ( foundLib && content[i] === ' ' ) {
+      break;
+    }
+  }
+
+  var libVarStr = libVar.join('').trim()
+
+  // console.log( libVarStr );
+
+  var startIndex = content.indexOf(libVarStr + '.' + fnQuery);
   var endIndex;
 
   var bracketCounter = {
@@ -16,9 +38,9 @@ module.exports = function(content, query) {
 
   // console.log(startIndex);
 
-  // console.log(content[startIndex + query.length]);
+  // console.log(content[startIndex + fnQuery.length]);
 
-  for ( var i = startIndex + query.length; i < content.length; i++ ) {
+  for ( var i = startIndex + fnQuery.length; i < content.length; i++ ) {
    if ( content[i] in bracketCounter ) {
      bracketCounter[ content[i] ]++;
    }
@@ -39,9 +61,9 @@ module.exports = function(content, query) {
 
 
   // console.log(bracketCounter);
-  console.log('startIndex:', startIndex);
-  console.log('endIndex:', endIndex);
-  console.log('snippit:', content.substring(startIndex, endIndex + 1));
+  // console.log('startIndex:', startIndex);
+  // console.log('endIndex:', endIndex);
+  // console.log('snippit:', content.substring(startIndex, endIndex + 1));
 
   if ( startIndex === -1 ) {
     return;
