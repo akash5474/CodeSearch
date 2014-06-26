49aa11dfa3ea589e0d05fb93f51c0e87d050ba51
buchanaf <buchanaf@gmail.com>
2014-06-26 14:43:41 -0400

    added collapse search

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,7 @@ angular.module('codeSearchApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
+  'ui.bootstrap',
   'ngRoute'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
