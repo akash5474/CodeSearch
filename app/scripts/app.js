6399096ff371567a0b5bc1aecc3ba00fab451f10
Akash Pannu <akash5474@gmail.com>
2014-06-26 10:52:51 -0400

    Moved about section into its own view and added its route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,12 +25,15 @@ angular.module('codeSearchApp', [
         controller: 'SettingsCtrl',
         authenticate: true
       })
+      .when('/about', {
+        templateUrl: 'partials/about',
+      })
       .otherwise({
         redirectTo: '/'
       });
-      
+
     $locationProvider.html5Mode(true);
-      
+
     // Intercept 401s and redirect you to login
     $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
       return {
@@ -50,7 +53,7 @@ angular.module('codeSearchApp', [
 
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$routeChangeStart', function (event, next) {
-      
+
       if (next.authenticate && !Auth.isLoggedIn()) {
         $location.path('/login');
       }
