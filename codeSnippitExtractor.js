3ba75bb0c909deae30f9c00e68ef857de5c55d20
Akash Pannu <akash5474@gmail.com>
2014-06-27 14:46:27 -0400

    Fixed search to exclude snippits we don't like

--- codeSnippitExtractor.js
+++ codeSnippitExtractor.js
@@ -4,7 +4,6 @@ var esprima = require('esprima');
 
 var extractSnippit = function(result) {
 
-
   var content = result.input;
   var startIndex = result.index;
   var endIndex;
@@ -23,6 +22,8 @@ var extractSnippit = function(result) {
    ']': 0
   };
 
+  // console.log(content[ startIndex + fnQuery.length ])
+
   for ( var i = startIndex + fnQuery.length; i < content.length; i++ ) {
     if ( content[i] in bracketCounter ) {
       bracketCounter[ content[i] ]++;
@@ -37,23 +38,29 @@ var extractSnippit = function(result) {
     }
   }
 
-  if ( startIndex === -1 ) {
+  if ( content[ startIndex + fnQuery.length ] !== '(' ) {
     return;
   } else {
+    console.log( content.substring(startIndex, endIndex + 1) );
     return content.substring(startIndex, endIndex + 1);
   }
 };
 
 
-module.exports = function(content, fnQuery) {
+module.exports = function(content, fnQuery, searchOptions) {
   var resultsArr = [];
   var parsedData = esprima.tokenize(content, {range: true});
   var methodMatches = fnQuery.match(/\./g);
+  // console.log('fnQuery', fnQuery);
 
-  if (methodMatches && methodMatches.length === 1 ) {
+  if (methodMatches && methodMatches.length === 1 &&
+      searchOptions.library === true &&
+      searchOptions.func === true) {
+
     var dotPosition = fnQuery.indexOf('.');
     var fnQueryPartOne = fnQuery.substring(0, dotPosition);
     var fnQueryPartTwo = fnQuery.substring(dotPosition+1);
+
     for (var i = 0; i <parsedData.length; i++) {
       var o = parsedData[i];
       if (o.type === 'Identifier' &&
@@ -66,24 +73,34 @@ module.exports = function(content, fnQuery) {
         fnQuery = fnQueryPartOne + '.' + fnQueryPartTwo;
         // console.log('extracting snippit', {input: content, index: parsedData[ i ].range[0], query: fnQuery});
         var snippit = extractSnippit({input: content, index: parsedData[ i ].range[0], query: fnQuery});
-        resultsArr.push(snippit);
+
+        if (snippit) {
+          resultsArr.push(snippit);
+        }
         // console.log('snippit', snippit);
       }
     }
   } else if ( !methodMatches ) {
     for ( var i = 0; i < parsedData.length; i++ ) {
       var o = parsedData[i];
-      if (o.type === 'Identifier' &&
-        o.value === fnQuery &&
-        parsedData[ i-1 ].type === 'Punctuator' &&
-        parsedData[ i-1 ].value === '.' &&
-        parsedData[ i-2 ].type === 'Identifier') {
+      if (searchOptions.library === false &&
+          searchOptions.func === true &&
+          o.type === 'Identifier' &&
+          o.value === fnQuery &&
+          parsedData[ i-1 ].type === 'Punctuator' &&
+          parsedData[ i-1 ].value === '.' &&
+          parsedData[ i-2 ].type === 'Identifier') {
 
         fnQuery = parsedData[ i-2 ].value + '.' + fnQuery;
         var snippit = extractSnippit({ input: content, index: parsedData[ i-2 ].range[0], query: fnQuery });
         // console.log('method snippit', snippit);
-        resultsArr.push(snippit);
-      } else if (o.type === 'Identifier' &&
+
+        if (snippit) {
+          resultsArr.push(snippit);
+        }
+      } else if (searchOptions.library === true &&
+                 searchOptions.func === false &&
+                 o.type === 'Identifier' &&
                  o.value === fnQuery &&
                  parsedData[ i+1 ].type === 'Punctuator' &&
                  parsedData[ i+1 ].value === '.' &&
@@ -92,7 +109,10 @@ module.exports = function(content, fnQuery) {
         fnQuery = fnQuery + '.' + parsedData[ i+2 ].value;
         var snippit = extractSnippit({ input: content, index: o.range[0], query: fnQuery });
         // console.log('library snippit', snippit);
-        resultsArr.push(snippit);
+
+        if (snippit) {
+          resultsArr.push(snippit);
+        }
       }
     }
   }
