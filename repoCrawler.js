f90322641481d0deead857c67c8e07b7af98a26f
Akash Pannu <akash5474@gmail.com>
2014-06-30 11:16:52 -0400

    repoCrawler promisified

--- repoCrawler.js
+++ repoCrawler.js
@@ -1,133 +1,180 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var exec = require('child_process').exec;
-// var async = require('async');
+var Promise = require("bluebird");
+var reqProm = Promise.promisify(request);
+var execProm = Promise.promisify(exec);
 
 var npmURL = 'https://www.npmjs.org';
 var startUrl = 'https://www.npmjs.org/browse/star';
 var starThresh = 50;
 
-var cloneRepo = function(cloneUrl) {
-  console.log('cloning', cloneUrl);
+var scrapeCloneUrl = function(response, body) {
+  if (response.statusCode === 200) {
+    var $ = cheerio.load(body);
+    var stars = +$('a.social-count.js-social-count').text();
+    // console.log('Found repo:', repoHref, 'Stars:', stars);
 
-  try {
-    process.chdir('reposToParse');
-    console.log('New directory: ' + process.cwd());
-  }
-  catch (err) {
-    console.log('chdir: ' + err);
-  }
-
-  exec('git clone ' + cloneUrl,
-   function(err, stdout, stderr) {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-      if (err !== null) {
-        console.log('exec error: ' + err);
-      }
-  });
-};
-
-var requestGithubAndClone = function(repoHref) {
-  request( repoHref, function(repoLinkErr, repoLinkRes, repoLinkBody) {
-
-    if ( !repoLinkErr && repoLinkRes.statusCode === 200 ) {
-
-      var $ = cheerio.load(repoLinkBody);
-      var stars = +$('a.social-count.js-social-count').text();
-      // console.log('Found repo:', repoHref, 'Stars:', stars);
-
-      if ( stars >= starThresh ) {
-        // Clone Repo
-        var cloneUrl = $('input.clone.js-url-field').val();
-        // console.log('Repo has more than 50 stars', cloneUrl );
-        cloneRepo(cloneUrl);
-
-      }
-    } else if ( repoLinkRes.statusCode === 404 ) {
-      // console.log('404 for repo:', repoHref);
+    if ( stars >= starThresh ) {
+      var cloneUrl = $('input.clone.js-url-field').val();
+      console.log('Repo has more than 50 stars', cloneUrl );
+      return cloneUrl;
     }
-  });
+  }
 };
 
-var requestNpmPage = function(pageHref) {
-  request(npmURL + pageHref,
-    function(depLinkErr, depLinkRes, depLinkBody) {
-      if ( !depLinkErr && depLinkRes.statusCode == 200 ) {
-        var $ = cheerio.load(depLinkBody);
+var scrapeRepoLink = function(response, body) {
+  if ( response.statusCode == 200 ) {
+    var $ = cheerio.load(body);
+    var repoLink;
 
-        var repoEl = $('.metadata a').filter(function() {
+    var repoEl = $('.metadata a').filter(function() {
 
-          var linkText = $(this).text();
-          var repoUrlSubstring = linkText.substring(linkText.length - 4);
-          return repoUrlSubstring === '.git';
+      var linkText = $(this).text();
+      var repoUrlSubstring = linkText.substring(linkText.length - 4);
+      return repoUrlSubstring === '.git';
 
-        }).each(function() {
-          var repoHref = $(this).attr('href');
-
-          requestGithubAndClone(repoHref);
-        });
-      }
+    }).each(function() {
+      repoLink = $(this).attr('href');
     });
+
+    return repoLink;
+  }
 };
 
-// var startUrl = 'https://www.npmjs.org/browse/depended/express';
+var scrapeStarredLinks = function(response, body) {
+  // console.log('body', response[0].body);
+  var $ = cheerio.load( body );
 
-var requestModuleDepended = function(dependedUrl) {
-  request(dependedUrl, function(err, res, body) {
-    if ( !err && res.statusCode == 200 ) {
+  var starredRepoLinks = $('.row a');
+  var paginationLinks = $('h1+ .description a');
 
-      var $ = cheerio.load(body);
+  var starredLinksArr = [];
+  var nextPageLink;
 
-      var dependentLinks = $('.row a');
-      var paginationLinks = $('h1+ .description a');
+  starredRepoLinks.each(function(idx, link) {
+    starredLinksArr.push( npmURL + '/browse/depended/' + $(this).text() );
+  });
 
-      dependentLinks.each(function(idx, el) {
-        var depHref = $(this).attr('href');
+  paginationLinks.each(function(idx, link) {
+    var text = $(this).text()
+                      .substring(0, $(this).text().length - 2);
 
-        requestNpmPage(depHref);
-      });
-
-      paginationLinks.each(function(idx, el) {
-        var text = $(this).text()
-                          .substring(0, $(this).text().length - 2);
-
-        if ( text === 'next' ) {
-          var nextHref = $(this).attr('href');
-          // console.log(nextHref);
-          requestModuleDepended(npmURL + nextHref);
-        }
-      });
+    if ( text === 'next' ) {
+      nextPageLink = npmURL + $(this).attr('href');
     }
   });
+
+  return [starredLinksArr, nextPageLink];
 };
 
-var requestStarred = function(starredUrl) {
-  request(starredUrl, function(err, res, body) {
+var scrapeDependedLinks = function(response, body) {
+  if ( response.statusCode == 200 ) {
     var $ = cheerio.load(body);
 
-    var starredRepoLinks = $('.row a');
-    var paginationLinks = $('h1+ .description a');
+    var moduleLinks = $('.row a');
+    var nextPageLinks = $('h1+ .description a');
 
-    starredRepoLinks.each(function(idx, el) {
-      var repo = $(this).text();
-      // console.log('https://www.npmjs.org/browse/depended/' + repo);
-      requestModuleDepended('https://www.npmjs.org/browse/depended/' + repo);
+    var moduleLinksArr = [];
+    var nextPageLink;
+
+    moduleLinks.each(function(idx, el) {
+      moduleLinksArr.push( npmURL + $(this).attr('href') );
     });
 
-    paginationLinks.each(function(idx, el) {
+    nextPageLinks.each(function(idx, el) {
       var text = $(this).text()
                         .substring(0, $(this).text().length - 2);
 
       if ( text === 'next' ) {
-        var nextHref = $(this).attr('href');
-        // console.log(nextHref);
-        requestStarred(npmURL + nextHref);
+        nextPageLink = npmURL + $(this).attr('href');
       }
     });
+  }
 
+  return [moduleLinksArr, nextPageLink];
+};
+
+var cloneRepo = function(cloneUrl) {
+  if ( !cloneUrl ) {
+    return;
+  }
+
+  console.log('cloning', cloneUrl);
+
+  if ( process.cwd().lastIndexOf('reposToParse') === -1 ) {
+    try {
+      process.chdir('reposToParse');
+      console.log('New directory: ' + process.cwd());
+    } catch (err) {
+      console.log('chdir: ' + err);
+    }
+  }
+
+  // return execProm('git clone ' + cloneUrl ).spread(function(stdout, stderr) {
+  //     console.log('stdout: ' + stdout);
+  //     console.log('stderr: ' + stderr);
+  // }).catch(function(e) {
+  //     console.log('exec error: ' + e);
+  // });
+};
+
+var getCloneUrl = function(repoHref) {
+  if ( !repoHref ) {
+    return;
+  }
+
+  return reqProm(repoHref).spread(scrapeCloneUrl)
+    .catch(function(err) {
+      console.log('ERROR requesting github', err, '\n', repoHref);
+    });
+};
+
+var getRepoUrl = function(pageLink) {
+  if ( !pageLink ) {
+    return;
+  }
+
+  return reqProm(pageLink).spread(scrapeRepoLink)
+    .catch(function(err) {
+      console.log('Err requesting NPM Page', err, '\n', pageLink);
+    });
+};
+
+var getDependingPage = function(dependedUrl) {
+  return reqProm(dependedUrl).spread(scrapeDependedLinks)
+    .spread(function(depLinks, nextPageLink) {
+      Promise.resolve(depLinks)
+        .map(getRepoUrl)
+        .map(getCloneUrl)
+        .map(cloneRepo, {concurrency: 5})
+        .then(function() {
+          if ( nextPageLink ) {
+            getDependingPage(nextPageLink);
+          }
+        });
   });
 };
 
-// requestModuleDepended(startUrl);
-requestStarred(startUrl);
+var getStarredPage = function(starredUrl) {
+  reqProm(starredUrl).spread(scrapeStarredLinks)
+    .spread(function(repoLinks, nextLink) {
+
+      return Promise.resolve(repoLinks)
+        .each(getDependingPage)
+        .then(function() {
+          if (nextLink) {
+            console.log('getStarredPage nextLink', nextLink);
+            getStarredPage(nextLink);
+          }
+        })
+        .catch(function(err) {
+          console.log('ERROR: getStarredPage promise resolve', err);
+        });
+  })
+  .catch(function(err) {
+    console.log('ERROR: getStarredPage', err);
+  });
+};
+
+getStarredPage(startUrl);
