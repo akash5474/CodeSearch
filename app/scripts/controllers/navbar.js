4a5630badf45db8d1dfc780c59eb0a1e79f5b3da
Akash Pannu <akash5474@gmail.com>
2014-07-02 19:26:11 -0400

    Updated navbar.js to redirect to proper place after logout

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -13,7 +13,7 @@ angular.module('codeSearchApp')
     $scope.logout = function() {
       Auth.logout()
       .then(function() {
-        $location.path('/login');
+        $location.path('/');
       });
     };
 
